38d6344a88b318601e40e0887ef473f26cc41f9d
patricio javier benavides monsalvez <patojavier1981@gmail.com>
2025-05-11 16:43:00 -0400

    despliegue

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -12,7 +12,7 @@ function Register() {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5000/api/auth/register', {
+      await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/register`, {
         name,
         email,
         password,
